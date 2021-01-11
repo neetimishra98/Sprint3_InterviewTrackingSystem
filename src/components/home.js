6e7586487c391b058dabcad79d711de4d9f3cfb9
Yash Kadam <yashk398@gmail.com>
2021-01-11 12:26:05 +0530

    Altered CSS, Folders and WARNING added

---
+++ src/components/home.js
@@ -0,0 +1,5 @@
+//Home Component
+//Inside this every service component could reside
+//Inside every xyz-service-component.js there would be components of their respective candidate, employee, panelmember and interview services from the above folders.
+//Do not change home.js or App.js while git based development without permissions, would lead to conflicts
+//Add this to WARNINGS.info once development starts 
