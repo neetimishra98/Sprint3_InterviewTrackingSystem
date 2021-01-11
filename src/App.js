9b4ef764ee53b237b52f01516064d7b8660eb140
Yash Kadam <yashk398@gmail.com>
2021-01-11 12:54:25 +0530

    Bootstrap fix - with imports in js files....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
+//Internal Components
+import HomeComponent from './components/home';
+
+//Installed Components
+import { Route, Link, Switch } from 'react-router-dom';
+//Bootstrap
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>INTERVIEW TRACKING SYSTEM</h1>
+      <HomeComponent></HomeComponent>
+        <Button href="#">Link</Button> <Button type="submit">Button</Button>{' '}
+        <Button as="input" type="button" value="Input" />{' '}
+        <Button as="input" type="submit" value="Submit" />{' '}
+        <Button as="input" type="reset" value="Reset" />
     </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -3,3 +3,13 @@
 //Inside every xyz-service-component.js there would be components of their respective candidate, employee, panelmember and interview services from the above folders.
 //Do not change home.js or App.js while git based development without permissions, would lead to conflicts
 //Add this to WARNINGS.info once development starts 
+
+const HomeComponent = () => {
+    return (
+        <div>.....</div>
+    );
+}
+
+
+
+export default HomeComponent
