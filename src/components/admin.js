aae15e868bea1bcda8974d3c0d44dfb467d2a73a
Suraj <surajmondal888@gmail.com>
2021-01-11 20:54:35 +0530

    Cancel interview action added

--- src/components/admin.js
+++ src/components/admin.js
@@ -2,8 +2,8 @@
 
 import EmployeeForAdmin from './employee/EmployeeForAdmin'
 import CandidateForAdmin from './candidate/CandidateForAdmin'
-import PanelMemberForAdmin from './interviewscheduler/InterviewSchedulerForAdmin'
-import InterviewSchedulerForAdmin from './panelmember/PanelMemberForAdmin'
+import PanelMemberForAdmin from './panelmember/PanelMemberForAdmin'
+import InterviewSchedulerForAdmin from './interviewscheduler/InterviewSchedulerForAdmin'
 
 // Bootstrap
 
--- src/components/interviewscheduler/InterviewSchedulerForAdmin.js
+++ src/components/interviewscheduler/InterviewSchedulerForAdmin.js
@@ -1,9 +1,39 @@
 //Interview Services Accessible By The Admin....
 
+import CancelInterview from './services/cancelinterview.js'
+
+import { Accordion, Card, Button } from 'react-bootstrap'
+
 const InterviewSchedulerForAdmin = () => {
     return (
         <div>
             {/* All underlying operations from services */}
+            <Accordion>
+                <Card>
+                    <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Search an Interview
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                    {/* Search an Employee component */}
+                    <Card.Body><CancelInterview></CancelInterview></Card.Body>
+                    {/* Search an Employee component */}
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                        Cancel Interview
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                    {/* Fetch all Employee component */}
+                    <Card.Body><div></div></Card.Body>
+                    {/* Fetch all Employee component */}
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
         </div>
     );
 }
