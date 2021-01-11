ce639a3ae845f9810b9ae75ad44df754ae05d923
neetimishra98 <neetimishra@github.com>
2021-01-11 18:42:31 +0530

    firstcommit

--- src/components/panelmember/PanelMemberForAdmin.js
+++ src/components/panelmember/PanelMemberForAdmin.js
@@ -10,14 +10,38 @@ const PanelMemberForAdmin = () => {
             <Accordion>
                 <Card>
                     <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                        Surrender As HR Panel
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                            Surrender As HR Panel
                     </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="0">
-                    {/* Search an Employee component */}
-                    <Card.Body><SurrenderAsHR></SurrenderAsHR></Card.Body>
-                    {/* Search an Employee component */}
+                        {/* Search an Employee component */}
+                        <Card.Body><SurrenderAsHR></SurrenderAsHR></Card.Body>
+                        {/* Search an Employee component */}
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Show All Panel Member
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                        {/* Fetch all Employee component */}
+                        <Card.Body><ShowPanelMembers></ShowPanelMembers></Card.Body>
+                        {/* Fetch all Employee component */}
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Delete Panel Member
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="2">
+                        {/* Fetch all Employee component */}
+                        <Card.Body><DeletePanelMember></DeletePanelMember></Card.Body>
+                        {/* Fetch all Employee component */}
                     </Accordion.Collapse>
                 </Card>
             </Accordion>
