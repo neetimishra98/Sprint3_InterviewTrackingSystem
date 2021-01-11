393e154ac6f27d43ad5e56f5edade699bb92d221
Sneha Chitale <snehachitale26@gmail.com>
2021-01-11 18:31:37 +0530

    admin

--- src/components/panelmember/PanelMemberForAdmin.js
+++ src/components/panelmember/PanelMemberForAdmin.js
@@ -1,9 +1,26 @@
 //Panel Services Accessible By The Admin....
+import SurrenderAsHR from './services/surrenderAsHr'
+
+import { Accordion, Card, Button } from 'react-bootstrap'
 
 const PanelMemberForAdmin = () => {
     return (
         <div>
             {/* All underlying operations from services */}
+            <Accordion>
+                <Card>
+                    <Card.Header>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                        Surrender As HR Panel
+                    </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                    {/* Search an Employee component */}
+                    <Card.Body><SurrenderAsHR></SurrenderAsHR></Card.Body>
+                    {/* Search an Employee component */}
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
         </div>
     );
 }
