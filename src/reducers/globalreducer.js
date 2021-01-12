443f22e1667347886f1fb0ec33c92070e0419e01
Priyanka-yd <74282467+Priyanka-yd@users.noreply.github.com>
2021-01-12 18:10:40 +0530

    Update globalreducer.js

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -2,12 +2,7 @@ const globalReducer = (state=[], action) => {
     switch(action.type){
         case 'SEARCH_EMPLOYEE': return action.payload;
 
-        //CASE TO VIEW CANDIDATE FOR HR
-        case 'VIEW_A_CANDIDATE_FOR_HR': return action.payload;
-
-        //CASE TO SURRENDER AS HR
-        case 'SURRENDER_AS_A_HR': return action.payload;
-    }
+      
 
 }
 
