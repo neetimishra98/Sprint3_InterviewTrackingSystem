5937bcd59df2803c139404ea3bda1f434ba934e4
Sneha Chitale <snehachitale26@gmail.com>
2021-01-12 18:41:18 +0530

    hr

--- src/actions/employee/searchemployee_action.js
+++ src/actions/employee/searchemployee_action.js
@@ -4,7 +4,7 @@ const SearchEmployeeAction = (props)=> {
     return async function(dispatch){
         let error;
         const axios = require('axios').default;
-        const url = 'http://localhost:8080/cgits/employee/'+props;
+        const url = 'http://localhost:9091/cgits/employee/'+props;
         const serverResponse = await axios.get(url) //await can only be used with async function....
         .then(response => dispatch({type: 'SEARCH_EMPLOYEE', payload: response}))
         .catch(error => console.log(error));
