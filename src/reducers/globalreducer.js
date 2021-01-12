ab563ecea1dbd01a9b71e5e6871bcd30f9d9e9f2
neetimishra98 <neetimishra@github.com>
2021-01-12 21:45:37 +0530

    commit

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -2,9 +2,11 @@ const globalReducer = (state=[], action) => {
     switch(action.type){
         case 'SEARCH_EMPLOYEE': return action.payload;
 
-      
+        
+        //CASE TO SHOW PANEL MEMBERS
+        case 'SHOW_PANELMEMBER': return action.payload;
 
-}
+    }
 }
 
 export default globalReducer;
