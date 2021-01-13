662842d05b06d139ec54285e1caf5e7ab189689a
Yash Kadam <yashk398@gmail.com>
2021-01-13 17:48:46 +0530

    Show Employee Full....

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -1,23 +1,12 @@
 const globalReducer = (state=[], action) => {
     switch(action.type){
         case 'SEARCH_EMPLOYEE': return action.payload;
+        case 'ADD_PANEL_MEMBER': return action.payload;
+        case 'SHOW_EMPLOYEES': return action.payload;
 
-        //CASE TO VIEW CANDIDATE FOR HR
-        case 'VIEW_A_CANDIDATE_FOR_HR': return action.payload;
+      
 
-        //CASE TO SURRENDER AS HR
-        case 'SURRENDER_AS_A_HR': return action.payload;
-
-        //default:
-           // return [];
-
-<<<<<<< HEAD
-        //case to cancel the Interview
-        case 'CANCEL_INTERVIEW': return action.payload;
-
-    }
-=======
-        }
->>>>>>> 365d39571a3e7471dc4c8e30deddb15a30692b3b
 }
+}
+
 export default globalReducer;
