d6d2cbc293094eae902d48755bc33e05bcf47461
Prasanna <prasannatm001@gmail.com>
2021-01-13 17:57:57 +0530

    givetechrating

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -7,16 +7,12 @@ const globalReducer = (state=[], action) => {
 
       
 
-<<<<<<< HEAD
         //case to give techrating
         case 'GIVE_TECH_RATING' : return action.payload;
 
         //case to view all members
         case 'VIEW_INTERVIEW_MEMBER_FOR_TECH': return action.payload;
 
-=======
-}
->>>>>>> 662842d05b06d139ec54285e1caf5e7ab189689a
 }
 }
 export default globalReducer;
