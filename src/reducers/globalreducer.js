4c981fdedf4603e53e3ebb5ad8e7d5804c68edeb
Yash Kadam <yashk398@gmail.com>
2021-01-13 15:36:12 +0530

    add panel member update actions

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -1,19 +1,12 @@
 const globalReducer = (state=[], action) => {
     switch(action.type){
         case 'SEARCH_EMPLOYEE': return action.payload;
-        
-        // JUST ADD ALL CASES OF REDUCER HERE....
+        //CASE TO SURRENDER AS HR
+        case 'ADD_PANEL_MEMBER': return action.payload;
 
-        //CASE TO SHOW PANEL MEMBERS
-        case 'SHOW_PANELMEMBER': return action.payload;
+      
 
-        //case to give techrating
-        case 'GIVE_TECH_RATING' : return action.payload;
-
-        //case to view all members
-        case 'VIEW_INTERVIEW_MEMBER_FOR_TECH': return action.payload;
-
-    }
+}
 }
 
 export default globalReducer;
