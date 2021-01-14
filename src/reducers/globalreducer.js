8dc26212468278cd0bfad9cb3db59141d9b65791
Sneha Chitale <snehachitale26@gmail.com>
2021-01-14 18:28:49 +0530

    surrender

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -1,15 +1,22 @@
-const globalReducer = (state=[], action) => {
+const globalReducer = (state= { panelmember: [], candidate: [] }, action) => {
     switch(action.type){
         case 'SEARCH_EMPLOYEE': return action.payload;
         case 'ADD_PANEL_MEMBER': return action.payload;
         case 'SHOW_EMPLOYEES': return action.payload;
 
         //CASE TO VIEW A CANDIDATE FOR HR
-        case 'VIEW_A_CANDIDATE_FOR_HR': return action.payload;
+        case 'VIEW_A_CANDIDATE_FOR_HR': 
+        //state.candidate = action.payload;
+        //console.log('VIEW_A_CANDIDATE_FOR_HR', state.candidate);
+        //return state;
+        return action.payload;
 
         //CASE TO SURRENDER AS HR
-        case 'SURRENDER_AS_HR': return action.payload;
-
+        case 'SURRENDER_AS_HR': 
+        //state.panelmember = action.payload;
+        //console.log('SURRENDER_AS_HR', state.panelmember);
+        //return state;
+        return action.payload;
 }
 }
 
