8b77ab1451c99a39b9e2dbd23587b5c8240f5743
Suraj <surajmondal888@gmail.com>
2021-01-14 20:11:55 +0530

    updated adminInterviewReducer added

--- src/reducers/adminInterviewReducer.js
+++ src/reducers/adminInterviewReducer.js
@@ -8,9 +8,6 @@ const InterviewReducer = (state=[], action) => {
          //case to cancel the Interview
          case 'CANCEL_INTERVIEW': return action.payload;
 
-        //Case to view interview member for tech
-        case 'VIEW_INTERVIEW_MEMBER_FOR_TECH': return action.payload;
-
       
     }
 
