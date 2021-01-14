f1112a7b090623846256e32c5509b15b2ef03867
Sneha Chitale <snehachitale26@gmail.com>
2021-01-14 09:51:18 +0530

    hr

--- src/reducers/globalreducer.js
+++ src/reducers/globalreducer.js
@@ -4,7 +4,11 @@ const globalReducer = (state=[], action) => {
         case 'ADD_PANEL_MEMBER': return action.payload;
         case 'SHOW_EMPLOYEES': return action.payload;
 
-      
+        //CASE TO VIEW A CANDIDATE FOR HR
+        case 'VIEW_A_CANDIDATE_FOR_HR': return action.payload;
+
+        //CASE TO SURRENDER AS HR
+        case 'SURRENDER_AS_HR': return action.payload;
 
 }
 }
